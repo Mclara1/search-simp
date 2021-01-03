896316e095130d046f29577e5ec1073ecc4272a2
Mclara1 <mclara1@ua.pt>
2021-01-03 16:22:05 +0000

    Respostas

--- src/components/AmadeusApi.js
+++ src/components/AmadeusApi.js
@@ -11,23 +11,16 @@ export default function Api(origem, destino, ida, volta) {
   return amadeus.shopping.flightOffersSearch.get({
     originLocationCode: origem,
     destinationLocationCode: destino,
-    departureDate: '2021-02-02', //mudar isto para ida
-    returnDate: '2021-03-02', //mudar isto para volta
+    departureDate: ida, 
+    returnDate: volta,
     adults: '1',
     currencyCode: 'EUR',
-    max: '30'
+    max: '5'
   }).then(function(response){
     return response.data;
-
-    // for (let index = 0; index < response.data.length; index++) {
-    //   const element = response.data[index];
-    //   var info = element.price.total
-    //   console.log(element.price.total);
-    // }
-
   }).catch(function(responseError){
     console.log(responseError.code);
-    alert("ERROR: Invalid code city. Try again.")
+    alert("ERROR: Invalid city or date. Try again.")
   });
 
 }
--- src/components/FlightResults.js
+++ src/components/FlightResults.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const FlightResults = ({apiData}) => {
+    console.log(apiData);
     return(
         <>
         {apiData && (
             apiData.map((resultado) => (
             <div>
-                <h1>Lista de voos</h1>
-                <p>{resultado.id}</p>
+                <h3>{resultado.itineraries[0].segments[0].departure.iataCode} ➔ {resultado.itineraries[0].segments[0].arrival.iataCode}</h3>
+                <h4>{resultado.itineraries[0].segments[0].arrival.at}</h4>
+                <h4>{resultado.itineraries[0].duration}</h4>
+                <h3>{resultado.itineraries[1].segments[0].departure.iataCode} ➔ {resultado.itineraries[1].segments[0].arrival.iataCode}</h3>
+                <h4>{resultado.itineraries[1].segments[0].arrival.at}</h4>
+                <h4>{resultado.itineraries[1].duration}</h4>
+                <h4>{resultado.price.total}€</h4>
+                <p>Seats left: {resultado.numberOfBookableSeats}</p>
             </div>))
         )}
 
